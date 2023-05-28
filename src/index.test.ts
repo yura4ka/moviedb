f02f9739140fdad46379bd63e72283945835780f
yura4ka <yura4ka05@gmail.com>
2023-05-28 20:44:06 +0300

    Add movie api tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,80 @@
-import { describe, it, expect } from 'vitest';
+import { GET_MOVIES, addMovie, changeMovie, removeMovie } from '$lib/utils/api';
+import { describe, expect, test } from 'vitest';
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
+describe('movie', () => {
+	let createdId: string;
+	const movieData = {
+		title: 'vitest movie',
+		description: 'vitest movie description',
+		year: 2023,
+		durationMinutes: 1,
+		image:
+			'https://m.media-amazon.com/images/M/MV5BOWIwZGY0OTYtZjUzYy00NzRmLTg5YzgtYWMzNWQ0MmZiY2MwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UY4096_.jpg',
+		trailer: 'https://www.youtube.com/embed/pBk4NYhWNMM',
+		mpaa: 'G' as 'G' | 'R',
+		rating: 2
+	};
+
+	test('create a movie', async () => {
+		const id = await addMovie({
+			...movieData,
+			genres: [
+				{ id: '8a3f323a-f5ba-4dac-85fd-977262c8cc8b' },
+				{ id: 'e2d2d772-cfa4-461d-8d01-0c0c47cb7de6' }
+			],
+			crew: [
+				{
+					personId: '313dcb9c-4c36-4d40-be3a-d736c1f6fc8c',
+					role: 'DIRECTOR'
+				},
+				{
+					personId: '654ad6d3-2765-4d7a-99cb-079914e7218d',
+					role: 'WRITER'
+				},
+				{
+					personId: '8cf7b0fb-a2e5-4bd1-9f1e-509a70d7b68d',
+					role: 'STAR'
+				}
+			]
+		});
+
+		expect(id).toBeDefined();
+		expect(id).toBeTypeOf('string');
+		createdId = id;
+	});
+
+	test('get created movie', async () => {
+		const result = await fetch(GET_MOVIES + `/${createdId}`).then((r) => r.json());
+		expect(result).toBeDefined();
+		expect(result).toMatchObject(movieData);
+		expect(result).toHaveProperty('id', createdId);
+		expect(result.genres).toHaveLength(2);
+		expect(result.crew).toHaveLength(3);
+	});
+
+	test('change movie', async () => {
+		const result = await changeMovie({ id: createdId, mpaa: 'R' });
+		expect(result).toBe(true);
+	});
+
+	test('check movie after change', async () => {
+		const result = await fetch(GET_MOVIES + `/${createdId}`).then((r) => r.json());
+		movieData.mpaa = 'R';
+
+		expect(result).toBeDefined();
+		expect(result).toMatchObject(movieData);
+		expect(result).toHaveProperty('id', createdId);
+		expect(result.genres).toHaveLength(2);
+		expect(result.crew).toHaveLength(3);
+	});
+
+	test('remove movie', async () => {
+		const result = await removeMovie(createdId);
+		expect(result).toBe(true);
+	});
+
+	test('movie is removed', async () => {
+		const result = await fetch(GET_MOVIES + `/${createdId}`).then((r) => r.json());
+		expect(result).toStrictEqual({ message: 'Bad request' });
 	});
 });
--- src/lib/utils/api.ts
+++ src/lib/utils/api.ts
@@ -1,5 +1,5 @@
 import type { z } from 'zod';
-import type { createMovieSchema } from './schemas';
+import type { changeMovieSchema, createMovieSchema } from './schemas';
 
 const HOST = 'http://localhost:5173/api/';
 
@@ -8,6 +8,7 @@ export const GET_GENRES = HOST + 'genres';
 export const GET_STARS = HOST + 'people';
 
 type TAddMovie = z.infer<typeof createMovieSchema>;
+type TChangeMovie = z.infer<typeof changeMovieSchema>;
 
 export async function addMovie(movie: TAddMovie) {
 	const result = await fetch(GET_MOVIES, {
@@ -21,5 +22,29 @@ export async function addMovie(movie: TAddMovie) {
 	let id = undefined;
 	if (result.ok) id = (await result.json()).id;
 
-	return id;
+	return id as string;
+}
+
+export async function changeMovie(movie: TChangeMovie) {
+	const result = await fetch(GET_MOVIES, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(movie)
+	});
+	const data = await result.json();
+	return result.ok && data.success === true;
+}
+
+export async function removeMovie(id: string) {
+	const result = await fetch(GET_MOVIES, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ id })
+	});
+	const data = await result.json();
+	return result.ok && data.success === true;
 }
